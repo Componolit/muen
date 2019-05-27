973df2cdb40f25cbf05fb379b083d8f6841d225c
Johannes Kliemann <kliemann@componolit.com>
2019-05-27 14:09:03 +0200

    trigger secondary stack

--- components/hello_world/src/hello_world.adb
+++ components/hello_world/src/hello_world.adb
@@ -4,7 +4,12 @@ with Debuglog.Client;
 
 procedure Hello_World
 is
+   function Hw return String
+   is
+   begin
+      return "Hello_World: Hello World!";
+   end Hw;
 begin
-   Debuglog.Client.Put_Line ("Hello_World");
+   Debuglog.Client.Put_Line (Hw);
    SK.CPU.Stop;
 end Hello_World;
