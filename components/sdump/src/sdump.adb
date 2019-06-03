256b396fa3f7852f0ec4364996811f3fd6d131e5
Johannes Kliemann <kliemann@componolit.com>
2019-06-03 16:33:54 +0200

    build with no -Wstack-usage and print name

--- components/sdump/src/sdump.adb
+++ components/sdump/src/sdump.adb
@@ -6,14 +6,13 @@ with SK.CPU;
 with Musinfo;
 with Musinfo.Instance;
 
-procedure Sdump
+procedure Sdump with
+   SPARK_Mode
 is
-   pragma Warnings (Off, "constant ""Name_Obj"" is not referenced");
    Name_Obj : constant Musinfo.Name_Type := Musinfo.Instance.Subject_Name;
 begin
    Componolit.Runtime.Debug.Log_Debug ("Sdump");
-   --  Componolit.Runtime.Debug.Log_Debug
-   --     ("Name: " & String (Name_Obj.Data (1 .. Integer (Name_Obj.Length))));
+   Componolit.Runtime.Debug.Log_Debug
+      ("Name: " & String (Name_Obj.Data (1 .. Integer (Name_Obj.Length))));
    SK.CPU.Stop;
-   pragma Warnings (On, "constant ""Name_Obj"" is not referenced");
 end Sdump;
