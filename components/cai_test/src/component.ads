d9ba57e03c208a381bdf3a48e1cb3a946fbd680a
Johannes Kliemann <kliemann@componolit.com>
2019-06-11 17:43:36 +0200

    fix typo

--- components/cai_test/src/component.ads
+++ components/cai_test/src/component.ads
@@ -6,7 +6,7 @@ package Component with
    SPARK_Mode
 is
 
-   procedure Construct (Capability: Componolit.Interfaces.Types.Capability);
+   procedure Construct (Capability : Componolit.Interfaces.Types.Capability);
    procedure Destruct;
 
    package Main is new Componolit.Interfaces.Component (Construct, Destruct);
